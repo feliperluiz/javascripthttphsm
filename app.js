991449c09123ee0fd9b00bbec3fbd7de58c6fa35
Felipe <lippee@gmail.com>
2019-04-04 01:55:11 -0300

    Adding TCP layer and raw socket

--- app.js
+++ app.js
@@ -1,28 +1,114 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-const ioclient = require('socket.io-client');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var tls = require('tls');
+var hashDocumentoAssinado = '';
+var hashDocumento = '';
 
+http.createServer(function (request, response) {
+console.log('Requisitando início do servidor node...');
 
-server.listen(3000);
+var filePath = '.' + request.url;
+if (filePath == './')
+    filePath = './index.html';
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+var extname = path.extname(filePath);
+var contentType = 'text/html';
+switch (extname) {
+    case '.js':
+        contentType = 'text/javascript';
+        break;
+    case '.css':
+        contentType = 'text/css';
+        break;
+}
+
+fs.readFile(filePath, function(error, content) {
+    if (error) {
+        if(error.code == 'ENOENT'){
+            fs.readFile('./404.html', function(error, content) {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            });
+        } else {
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            response.end(); 
+        }
+    } else {
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
+    }
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+}).listen(4000);
+console.log('Servidor rodando em http://127.0.0.1:4000/');
+
+//Criando servidor WebTCP que será o Proxy para todas as requisições TCP dos clientes
+
+var WebTCP = require('./webtcp/lib/server/webtcp.js').WebTCP
+
+var serverTCP = new WebTCP();
+serverTCP.listen(4002, "localhost");
+
+
+//Criando socket interno para envio do hash da assinatura (partindo do cliente) e utilizando 
+
+var net = require('net');
+var server = net.createServer(function (socket) {
+  socket.write("Enviando mensagem do socket interno para o cliente\r\n");
+  socket.pipe(socket);
 });
 
-console.log('test');
-ioclient('wss://192.168.105.9:5696', { 
-      path: '/kmip',
-      key: './keykmip.pem',
-      cert: './certkmip.pem',
-      ca: './certificado_hsm.pem',
-      transports:  ['websocket'],
-      rejectUnauthorized: false
-   });
+server.listen(4001, "localhost", function() {
+  address = server.address();
+  console.log("Aberto servidor para socket interno %j", address);
+});
+
+server.on('connection', function (stream) {
+  console.log('Nova conexão de socket interno ' + stream.remoteAddress);
+});
+
+server.on('data', function (data) {
+  hashDocumento = data;
+  console.log('Dado recebido: ' + hashDocumento);
+});
+
+if (hashDocumentoAssinado !== '')
+    socket.write(hashDocumentoAssinado);
+
+/////////////////////////
+
+const options = {
+    ca: fs.readFileSync('certhsm.pem'),
+    key: fs.readFileSync('keykmip.pem'),
+    cert: fs.readFileSync('certkmip.pem'),
+    host: '192.168.105.9',
+    port: 5696,
+    rejectUnauthorized:false,
+    requestCert:true
+};
+
+const socket = tls.connect(options, () => {
+    console.log('client connected', socket.authorized ? 'authorized' : 'unauthorized');
+    process.stdin.pipe(socket);
+    process.stdin.resume();
+});
+
+socket.setEncoding('utf8');
+if (hashDocumento !== '')
+    socket.write(hashDocumento);
+
+socket.on('data', (data) => {
+    hashDocumentoAssinado = data;
+    console.log('Assinatura realizada recebida: ' + hashDocumentoAssinado);
+});
+
+socket.on('error', (error) => {
+    console.log(error);
+});
+
+socket.on('end', (data) => {
+    console.log('Socket end event');
+});
+
