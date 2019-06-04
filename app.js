e5fb05d4e900fa5c6aaa025388d0c2dbb296098b
Felipe <lippee@gmail.com>
2019-06-04 00:34:50 -0300

    HTTP Request to remote HSM

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var tls = require('tls');
+var https = require('https')
+const dado = JSON.stringify({
+  'len': '16'
+})
 
-// Instalação dos componentes para conexão com o Python
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request-promise');
-var app = express();
- 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// SOCKET STREAM
-// var ss = require('socket.io-stream');
-// var io = require('socket.io-client');
-// var socket = io.connect('192.168.105.9:5696');
-// var stream = ss.createStream();
-//
-
-var documentoBinarioAssinado = '';
 var documentoBinario = '';
 
 http.createServer(function (request, response) {
-    console.log('Requisitando início do servidor node...');
-
     var filePath = '.' + request.url;
     if (filePath == './')
         filePath = './index.html';
@@ -38,7 +22,8 @@ http.createServer(function (request, response) {
         case '.css':
             contentType = 'text/css';
             break;
-}
+    }
+
 
 fs.readFile(filePath, function(error, content) {
     if (error) {
@@ -61,84 +46,44 @@ fs.readFile(filePath, function(error, content) {
 }).listen(4000);
 console.log('(4000-NODE) Servidor rodando em http://127.0.0.1:4000/');
 
+
 //Criação do WebSocket para comunicação com o cliente Browser
-var WebSocketServer = require('ws').Server,
+var WebSocketServer = require('ws').Server, 
     wss = new WebSocketServer({port: 4001})
-
+    
     wss.on('connection', function (ws) {
-      ws.on('message', function (message) {
-            documentoBinario = message;            
-            console.log("Documento recebido do cliente para ser assinado: " + documentoBinario);
-
-            var options = {
-                method: 'POST',
-                uri: 'http://localhost:5000/sign',
-                body: documentoBinario
-            };
-            
-            var returndata;
-            var sendrequest = request(options)
-            .then(function (parsedBody) {
-                console.log(parsedBody); // parsedBody contains the data sent back from the Flask server
-                returndata = parsedBody; // do something with this data, here I'm assigning it to a variable.
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-
-            //res.send(returndata);
-
-        });
+      ws.on('message', function (message) {        
+            documentoBinario = message;
+      });
     })
 
+var options = { 
+    hostname: 'hsmlab64.dinamonetworks.com',
+    method: 'POST',
+    port: 443,
+    path: '/api/gen_rand',
+    passphrase: '12345678',
+    key: fs.readFileSync('./certsjson/lab.pri'), 
+    cert: fs.readFileSync('./certsjson/lab.cer'), 
+    ca: fs.readFileSync('./certsjson/hsm.cer'), 
+    checkServerIdentity: () => { return null; },
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': dado.length
+    }
+}; 
 
+var req = https.request(options, function(res) { 
+    res.on('data', function(data) { 
+        console.log('passou aqui' + data)
+        process.stdout.write(data); 
+    }); 
+}); 
 
-    /*// Informações sobre a comunicação com o TLS
-            
-cd C:\Users\Felipe\Desktop\TCC\reporter\reporter
-cd C:\Users\Felipe\Desktop\TCC\signaturekmip
+req.on('error', (e) => {
+  console.error('Erro:' + e);
+});
 
-            const options = {
-                ca: fs.readFileSync('certhsm.pem'),
-                key: fs.readFileSync('keykmip.pem'),
-                cert: fs.readFileSync('certkmip.pem'),
-                host: '192.168.105.9',
-                port: 5696,
-                rejectUnathorized: true,
-                //Error [ERR_TLS_CERT_ALTNAME_INVALID]: Hostname/IP does not match certificate's altnames: IP: 192.168.105.9 is not in the cert's list:
-                // If not false the server will reject any connection which is not 
-                // authorized with the list of supplied CAs. This option only has an effect if requestCert is true. 
-                requestCert: true,
-                checkServerIdentity: () => { return null; }
-            };
+req.write(dado);
+req.end();
 
-            // //Criação do TLS para conexão com o HSM
-
-            var socket = tls.connect(options, () => {
-                console.log('(5696-SOCKET) Conexão ao HSM: ', socket.authorized ? 'authorized' : 'unauthorized');
-                process.stdin.pipe(socket);
-                process.stdin.resume();
-            });
-
-            socket.setEncoding('utf8');
-                
-            if (documentoBinario !== '') {
-                console.log('(5696-SOCKET) Tem hash documento para enviar pro HSM!');
-                //console.log('Documento enviado ao HSM: '+documentoBinario+'\n');
-
-                //ss(socket).emit('sign', stream, documentoBinario);
-                console.log('Enviando uma assinatura!')
-                console.log('Assinatura do cliente: ' + documentoBinario)
-                console.log('Assinatura do cliente enviada: ' + Buffer.from(documentoBinario, 'hex'))
-                const buf2 = Buffer.from(documentoBinario, 'hex');
-                socket.write(buf2+'\n');
-            }
-                
-            socket.on('data', (data) => {
-                documentoBinarioAssinado = data;
-                console.log('Recebeu uma assinatura!')
-                console.log('Assinatura: ' + documentoBinarioAssinado)
-                //console.log('(5696-SOCKET) Assinatura realizada recebida: ' + documentoBinarioAssinado);
-
-                //Enviando documento em binário assinado para o cliente tratar              
-                ws.send(documentoBinarioAssinado);*/
