6a9ddb22b16a9f267e38c2d336ae4c2f6c623930
Felipe <lippee@gmail.com>
2019-08-21 19:55:36 -0300

    Alterações para demonstração

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var https = require('https')
-//var dadoStr = '{"usr": "master","pwd": "12345678"}'
-var dadoStr = '{"len": 16}'
-var documentoBinario = '';
+var respostaWeb = '';
+var authToken = '';
 
 http.createServer(function (request, response) {
     var filePath = '.' + request.url;
@@ -21,25 +20,23 @@ http.createServer(function (request, response) {
             contentType = 'text/css';
             break;
     }
-
-
-fs.readFile(filePath, function(error, content) {
-    if (error) {
-        if(error.code == 'ENOENT'){
-            fs.readFile('./404.html', function(error, content) {
-                response.writeHead(200, { 'Content-Type': contentType });
-                response.end(content, 'utf-8');
-            });
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
         } else {
-            response.writeHead(500);
-            response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            response.end(); 
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
         }
-    } else {
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(content, 'utf-8');
-    }
-});
+    });
 
 }).listen(4000);
 console.log('(4000-NODE) Servidor rodando em http://127.0.0.1:4000/');
@@ -50,40 +47,71 @@ var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 4001})
     
     wss.on('connection', function (ws) {
-      ws.on('message', function (message) {        
-            documentoBinario = message;
-      });
+      ws.on('message', function (message) { 
+            var pathValue = ''
+            var dadoStr = ''
+            var headersObject = ''
+            
+            if (parseInt(message)) {
+                pathValue = '/api/gen_rand'
+                dadoStr = '{"len": '+message+'}'
+                headersObject = {
+                    'Content-Type': 'application/json',
+                    'Content-Length': dadoStr.length,
+                    'Authorization': 'HSM' + authToken
+                }
+            } else {
+                pathValue = '/api/auth'
+                dadoStr = '{"usr": "master","pwd": "12345678"}'
+                headersObject = {
+                    'Content-Type': 'application/json',
+                    'Content-Length': dadoStr.length
+                }
+            }
+    
+            var options = { 
+                hostname: 'hsmlab64.dinamonetworks.com',
+                method: 'POST',
+                port: 443,
+                path: pathValue,
+                passphrase: '12345678',
+                key: fs.readFileSync('./certsjson/lab.pri'), 
+                cert: fs.readFileSync('./certsjson/lab.cer'), 
+                ca: fs.readFileSync('./certsjson/hsm.cer'), 
+                rejectUnauthorized: false,
+                headers: headersObject
+            }; 
+
+            var req = https.request(options, function(res) { 
+                res.on('data', function(data) { 
+                    var resposta = JSON.parse(data)
+                    if (resposta.token !== undefined) {
+                        authToken = resposta.token
+                        respostaWeb = authToken
+                        console.log('Passou na resposta do token: ' + respostaWeb)
+                    } else {
+                        console.log(resposta)
+                        respostaWeb = resposta.rnd
+                        console.log('Resposta do numero aleatorio: ' + respostaWeb)                     
+                    }
+                    ws.send(respostaWeb);
+                }); 
+
+                req.on('error', (e) => {
+                    console.error('Erro:' + e);
+                });
+            }); 
+            req.write(dadoStr);
+            req.end();
+        });
+
+        
     })
 
-var options = { 
-    hostname: 'hsmlab63.dinamonetworks.com',
-    method: 'POST',
-    port: 443,
-    path: '/api/gen_rand',
-    passphrase: '12345678',
-    key: fs.readFileSync('./certsjson/lab.pri'), 
-    cert: fs.readFileSync('./certsjson/lab.cer'), 
-    ca: fs.readFileSync('./certsjson/hsm.cer'), 
-    rejectUnauthorized: false,
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': dadoStr.length,
-        'Authorization': 'HSM EA90DCD52172616D33F8D4A1DABFAE70B158F8431DBCC880FF5D8B327503E816'
-    }
-}; 
 
-var req = https.request(options, function(res) { 
-    res.on('data', function(data) { 
-        process.stdout.write(data); 
-    }); 
-}); 
 
-req.on('error', (e) => {
-  console.error('Erro:' + e);
-});
 
-req.write(dadoStr);
-req.end();
+
 
 //{ "token":  "48D50D8E79ABBEFC5F711B4B517046B29E4F7D52229E1587DB570752E13A081A" , "cid": 935639491, "pwd_expired": 0}
 // { "rnd": "D98A09FFF997DB8197EFB67CDC23E142"}
--- js/controller.js
+++ js/controller.js
@@ -1,30 +1,27 @@
 var hashDocumentoAssinado = '';
 var hashDocumento = '';
+var ws = '';
 
-function onChange(event) { 
-    var logFile = $('#input_file').get(0).files[0];
-    var readerFile = new FileReader();
-    readerFile.readAsBinaryString(logFile);
-    readerFile.onload = function(event){
-        var arrayBuffer = event.target.result;
-        hashDocumento = CryptoJS.SHA256(arrayBuffer).toString(CryptoJS.enc.Hex);
-        console.log(hashDocumento);
-             
-    }
+function Auth () { 
+  ws = new WebSocket('ws://localhost:4001'); 
+  ws.onopen = function () {
+      ws.send(true)
+      ws.onmessage = function(dado) {
+        console.log('Token: ' + dado.data);
+      };
+  };
 }
 
-function Sign () {  
-  var ws = new WebSocket('ws://localhost:4001');
-
-ws.onopen = function () {
-  var teste = "teste"
-  ws.send(teste)
-  console.log('Valor de assinatura enviado');
-};
-
-ws.onmessage = function(data) {
-  hashDocumentoAssinado = data;
-  console.log('Documento em binário assinado: ' + hashDocumentoAssinado.data);
-};
-
+function GenerateRandom () { 
+    ws = new WebSocket('ws://localhost:4001');
+    ws.onopen = function () {
+      ws.send(16) //16 bytes aleatórios gerados
+      ws.onmessage = function(dado) {
+        console.log('Número aleatório gerado: ' + dado.data);
+      };
+    };
 }
+
+
+
+
